2006cf773efb8acbc8a15116c01ea9a2ede8f754
coverosu <coverosu@gmail.com>
2022-12-27 17:08:03 -0800

    added command line argument handling for project path in main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 """Auto Commit Name Generator: Generates a unique git commit name based on the changes made to a repository"""
+import os
 import subprocess
+import sys
+from pathlib import Path
 
 from chatgpt_wrapper import ChatGPT
 
 
 def main() -> int:
+    args = sys.argv[1:]
+
+    if not args:
+        project_path = Path("./").absolute()
+    else:
+        project_path = Path(args[0]).absolute()
+
+    os.chdir(str(project_path))
 
     output = subprocess.run(
         ["git", "diff", "HEAD"],
@@ -14,7 +25,7 @@ def main() -> int:
 
     assert not output.stderr, "Error occurred when trying to get git difference"
 
-    assert not output.stdout, "No output was provided."
+    assert output.stdout, "No output was provided."
 
     chatgpt = ChatGPT()
 
